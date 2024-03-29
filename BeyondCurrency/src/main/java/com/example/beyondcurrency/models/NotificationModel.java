02e6d71a57f7eb2f3a8c4d4ee00b807a37971eae
Ryan <ab00999819@gmail.com>
2024-03-29 02:17:57 -0700

    request repo 30%

--- BeyondCurrency/src/main/java/com/example/beyondcurrency/models/NotificationModel.java
+++
@@ -1,8 +0,0 @@
-package com.example.beyondcurrency.models;
-
-public class NotificationModel {
-
-    private int notificationId;
-    private int userId;
-    private String content;
-}
--- BeyondCurrency/src/main/java/com/example/beyondcurrency/models/SavedTalentModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.beyondcurrency.models;
-
-public class SavedTalentModel {
-    private int id;
-    private int userId;
-    private int savedTalentId;
-}
